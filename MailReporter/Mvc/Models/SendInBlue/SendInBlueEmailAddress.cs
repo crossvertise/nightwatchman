d83d878f35ba6069ee95e83c7f99c9e6c9b00ce3
Anuj Angooral <anujangooral@gmail.com>
2022-05-04 11:02:14 +0530

    Removed hard coded emails

--- MailReporter/Mvc/Models/SendInBlue/SendInBlueEmailAddress.cs
+++ MailReporter/Mvc/Models/SendInBlue/SendInBlueEmailAddress.cs
@@ -9,14 +9,6 @@
             Staudigl = new Hashtable();
             ApprovedStaudigl = new Hashtable();
 
-            Staudigl.Add("druckfreigabe-webhook@replydev.crossvertise.com", "druckfreigabe-webhook@replydev.crossvertise.com");
-            Staudigl.Add("druckfreigabe-webhook-qa@replydev.crossvertise.com", "druckfreigabe-webhook-qa@replydev.crossvertise.com");
-            Staudigl.Add("druckfreigabe-webhook-staging@replydev.crossvertise.com", "druckfreigabe-webhook-staging@replydev.crossvertise.com");
-            Staudigl.Add("druckfreigabe-webhook-demo@replydev.crossvertise.com", "druckfreigabe-webhook-demo@replydev.crossvertise.com");
-
-            ApprovedStaudigl.Add("staudigl-approval-qa@replydev.crossvertise.com", "staudigl-approval-qa@replydev.crossvertise.com");
-            ApprovedStaudigl.Add("staudigl-approval-staging@replydev.crossvertise.com", "staudigl-approval-staging@replydev.crossvertise.com");
-            ApprovedStaudigl.Add("staudigl-approval@replydev.crossvertise.com", "staudigl-approval@replydev.crossvertise.com");
         }
 
         public static Hashtable Staudigl { get; set; }
